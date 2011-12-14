f330c28a3c8a3673a5cce52d3e6a1b33aae34f2c
unknown <artem@artem-laptop.(none)>
2011-12-15 03:00:24 +0400

    rename getter from 'getArtists' to 'artists'

--- src/main/scala/sh/topartist/LineUp.scala
+++ src/main/scala/sh/topartist/LineUp.scala
@@ -1,28 +1,28 @@
 package sh.topartist
 
 class LineUp {
-  private val artists = scala.collection.mutable.Map[String, Artist]()
+  private val _artists = scala.collection.mutable.Map[String, Artist]()
 
   def getArtist(name: String): Artist = {
-    artists(name)
+    _artists(name)
   }
 
-  def getArtists: Iterable[Artist] = {
-    artists.values
+  def artists: Seq[Artist] = {
+    _artists.values.toSeq
   }
 
   def addPerformance(name: String, when: String, where: String) {
     val performance = new Performance(when, where)
 
-    artists.get(name) match {
+    _artists.get(name) match {
       case Some(artist) => artist.addPerformance(performance)
       case None => {
         val artist = new Artist(name)
         artist.addPerformance(performance);
-        artists += name -> artist
+        _artists += name -> artist
       }
     }
   }
 
-  override def toString = artists.toString()
+  override def toString = _artists.toString()
 }
