b2bfa0adc235011702f5b86350b58e180b5bb515
unknown <artem@artem-laptop.(none)>
2011-12-15 03:06:30 +0400

    rename getter from 'getArtists' to 'artists'

--- src/test/scala/sh/topartist/festival/parser/Kazantip19LineUpParserTest.scala
+++ src/test/scala/sh/topartist/festival/parser/Kazantip19LineUpParserTest.scala
@@ -8,17 +8,17 @@ class Kazantip19LineUpParserTest extends FunSuite {
 
   test("parseLineUp(), incorrect line-up format, no 'when'") {
     val lineUp = new Kazantip19LineUpParser().parseLineUp("""  - Cosmonaut (SUNSET Point )""")
-    assert(0 === lineUp.getArtists.size)
+    assert(0 === lineUp.artists.size)
   }
 
   test("parseLineUp(), incorrect line-up format, no 'who'") {
     val lineUp = new Kazantip19LineUpParser().parseLineUp("""31,07 - (SUNSET Point )""")
-    assert(0 === lineUp.getArtists.size)
+    assert(0 === lineUp.artists.size)
   }
 
   test("parseLineUp(), incorrect line-up format, no 'where'") {
     val lineUp = new Kazantip19LineUpParser().parseLineUp(""" 1.2 - Cosmonaut ()""")
-    assert(0 === lineUp.getArtists.size)
+    assert(0 === lineUp.artists.size)
   }
   
   test("parseLineUp() with different djs") {
@@ -30,7 +30,7 @@ class Kazantip19LineUpParserTest extends FunSuite {
     7,08 - GVOZD (CLOUDS) STEPPIN SESSION / RESPECT FAMILY
     15,08 - Anrilov / B-Voice (SUNSET point) AMSTERDANCE""")
 
-    assert(6 === lineUp.getArtists.size)
+    assert(6 === lineUp.artists.size)
     assert(Set(new Performance("31,07", "SUNSET Point")) === lineUp.getArtist("Cosmonaut").performances)
     assert(Set(new Performance("5,08", "SUNSET Point")) === lineUp.getArtist("Loveski").performances)
     assert(Set(new Performance("5,08", "MAIN")) === lineUp.getArtist("Soundwalk Collective").performances)
@@ -44,13 +44,13 @@ class Kazantip19LineUpParserTest extends FunSuite {
     31,07 - Cosmonaut (SUNSET Point )
     5,08 - Cosmonaut (MAIN)""")
 
-    assert(1 === lineUp.getArtists.size)
+    assert(1 === lineUp.artists.size)
     assert(Set(new Performance("31,07", "SUNSET Point"), new Performance("5,08", "MAIN")) === lineUp.getArtist("Cosmonaut").performances)
   }
 
   ignore("parseLineUp() with 'kazantip-19-lineup_full.txt'") {
     val lineUp = new Kazantip19LineUpParser().parseLineUp(getClass.getResource("./test/resources/kazantip-19-lineup_full.txt"))
-    assert(199 === lineUp.getArtists.size)
+    assert(199 === lineUp.artists.size)
     println(lineUp)
   }
 }
