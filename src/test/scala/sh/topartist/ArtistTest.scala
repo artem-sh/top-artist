e45ec68aeacb0c91f693802f796e3e28c1cc030d
unknown <artem@artem-laptop.(none)>
2011-12-17 00:41:09 +0400

    add test to check natural Artists sorting

--- src/test/scala/sh/topartist/ArtistTest.scala
+++ src/test/scala/sh/topartist/ArtistTest.scala
@@ -6,14 +6,14 @@ import sh.topartist.rating.promodjru.PromodjRuRating
 
 
 class ArtistTest extends FunSuite {
-  val artistName = "Кино"
-  val artist = new Artist(artistName)
-
   test("name, getter") {
+    val artistName = "Кино"
+    val artist = new Artist(artistName)
     assert(artistName === artist.name)
   }
 
   test("addPerformance()") {
+    val artist = new Artist("Ivan")
     val performance1 = new Performance("2.4", "kazantip MAIN")
     val performance2 = new Performance("2.3", "MAIN")
     artist.addPerformance(performance1)
@@ -29,11 +29,28 @@ class ArtistTest extends FunSuite {
     assert(artist1 < artist2)
   }
 
+  test("naturally sorted (by lastFmRating)") {
+    val a5 = createArtist(5)
+    val a1 = createArtist(1)
+    val a10 = createArtist(10)
+
+    assert(List(a1, a5, a10) === List(a5, a1, a10).sorted)
+    assert(List(a1, a5, a10) === List(a1, a10, a5).sorted)
+    assert(List(a1, a5, a10) === List(a10, a5, a1).sorted)
+    assert(List(a1, a1, a10) === List(a10, a1, a1).sorted)
+  }
+
   test("compareByPromodjRuRating()") {
     val artist1 = new Artist("a1")
     artist1.totalRating.promodjRuRating = PromodjRuRating(10)
     val artist2 = new Artist("a2")
     artist2.totalRating.promodjRuRating = PromodjRuRating(20)
     assert(artist1.compareByPromodjRuRating(artist2) < 0)
+  }
+
+  private def createArtist(lastFmListenersCount: Int): Artist = {
+    val artist = new Artist("a" + lastFmListenersCount)
+    artist.totalRating.lastFmRating = LastFmRating("a", lastFmListenersCount)
+    artist
   }
 }
