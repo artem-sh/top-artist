9dfe423d8e741ade3939b7307aea79702870934b
unknown <artem@artem-laptop.(none)>
2011-12-20 02:31:02 +0400

    dumb app to gather artists' ratings

--- src/main/scala/sh/topartist/App.scala
+++ src/main/scala/sh/topartist/App.scala
@@ -1,8 +1,19 @@
 package sh.topartist
 
+import rating.lastfm.LastFmRetriever
+import rating.promodjru.PromodjRuRetriever
 import sh.topartist.festival.Kazantip
+import sh.topartist.util.raisin.Raisin._
 
 
 object App extends scala.App {
-//  Kazantip.rateArtists()
+  val lineUpStr = """31,07 - Cosmonaut (SUNSET Point )
+      6,08 - Pendulum + MC WREK (MAIN STAGE) OPENING CEREMONY
+      20,08 - Groove Armada / ANDY CATO (MAIN STAGE) CLOSING PARTY"""
+
+  using(new LastFmRetriever, new PromodjRuRetriever) {
+    (lastFmRetriever, promodjRuRetriever) =>
+      val lineUp = Kazantip.rateArtists(lineUpStr, lastFmRetriever, promodjRuRetriever)
+      lineUp.artists.sortWith(_ > _).foreach(println)
+  }
 }
