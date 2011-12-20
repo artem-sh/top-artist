9f212d59a1df9d285089e42a362d82623c7f8fde
unknown <artem@artem-laptop.(none)>
2011-12-21 03:45:54 +0400

    VkontakteRuParser implementation started

---
+++ src/main/scala/sh/topartist/rating/vkontakteru/VkontakteRuParser.scala
@@ -0,0 +1,9 @@
+package sh.topartist.rating.vkontakteru
+
+import net.liftweb.json._
+
+object VkontakteRuParser {
+  def parseRating(audioSearchJsonResponse: JValue): Option[VkontakteRuRating] = {
+    null
+  }
+}
---
+++ src/test/scala/sh/topartist/rating/vkontakteru/VkontakteRuParserTest.scala
@@ -0,0 +1,30 @@
+package sh.topartist.rating.vkontakteru
+
+import org.scalatest.FunSuite
+import net.liftweb.json._
+import sh.topartist.rating.vkontakteru.VkontakteRuParser._
+
+class VkontakteRuParserTest extends FunSuite {
+  ignore("parseTracksCount(), no tracks found") {
+    assert(None === parseRating(parse("""{"response":[0]}""")))
+  }
+
+  ignore("parseTracksCount(), tracks found") {
+    assert(VkontakteRuRating(454221) === parseRating(parse("""
+    {
+      "response": [
+        454221,
+        {
+          "aid": "72583074",
+          "owner_id": "3508708",
+          "artist": "The Prodigy",
+          "title": "Omen",
+          "duration": "216",
+          "url": "http:\/\/cs4589.vkontakte.ru\/u3508708\/audio\/4c3f5da968c5.mp3",
+          "lyrics_id": "3690013"
+        }
+      ]
+    }
+    """)))
+  }
+}
