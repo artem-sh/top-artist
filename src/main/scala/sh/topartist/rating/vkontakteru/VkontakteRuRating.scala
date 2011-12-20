825074188f8a124bf909d957fe221a32239e4e5a
unknown <artem@artem-laptop.(none)>
2011-12-21 03:23:10 +0400

    simple renaming

--- src/main/scala/sh/topartist/rating/vkontakteru/VkontakteRuRating.scala
+++ src/main/scala/sh/topartist/rating/vkontakteru/VkontakteRuRating.scala
@@ -3,11 +3,11 @@ package sh.topartist.rating.vkontakteru
 import sh.topartist.rating.Rating
 
 
-case class VkontakteRuRating(foundTracksCount: Int) extends Rating {
+case class VkontakteRuRating(tracksCount: Int) extends Rating {
   override def +(that: Rating): VkontakteRuRating =
-    new VkontakteRuRating(this.foundTracksCount + that.asInstanceOf[VkontakteRuRating].foundTracksCount)
+    new VkontakteRuRating(this.tracksCount + that.asInstanceOf[VkontakteRuRating].tracksCount)
 
-  override def compare(that: Rating) = this.foundTracksCount - that.asInstanceOf[VkontakteRuRating].foundTracksCount
+  override def compare(that: Rating) = this.tracksCount - that.asInstanceOf[VkontakteRuRating].tracksCount
 }
 
 object VkontakteRuRating {
