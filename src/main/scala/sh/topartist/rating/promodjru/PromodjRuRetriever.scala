b5d213362e014bb0346dc20a0bac20dbe174c624
Artem Shiyanov <shiyanov@gmail.com>
2011-12-26 15:26:17 +0400

    finish with throwing exceptions from rating parsers

--- src/main/scala/sh/topartist/rating/promodjru/PromodjRuRetriever.scala
+++ src/main/scala/sh/topartist/rating/promodjru/PromodjRuRetriever.scala
@@ -2,24 +2,26 @@ package sh.topartist.rating.promodjru
 
 import org.jsoup.Jsoup
 import dispatch._
-import sh.topartist.rating.RatingRetriever
+import sh.topartist.rating.{RatingParserException, RatingRetriever}
+import sh.topartist.rating.promodjru.PromodjRuRatingParser._
 
 
 class PromodjRuRetriever(http: Http) extends RatingRetriever {
   private val httpClient = http
 
   override def retrieveRating(artistName: String): PromodjRuRating = {
-    findDjPageUrl(artistName) match {
-      case Some(djPageUrl) => {
-        PromodjRuRatingParser.parseRating(httpClient(url(djPageUrl) as_str))
+    try {
+      findDjPageUrl(artistName) match {
+        case Some(djPageUrl) => parseRating(httpClient(url(djPageUrl) as_str))
+        case None => PromodjRuRating.Unknown
       }
-      case None => PromodjRuRating.Unknown
+    } catch {
+      case e: RatingParserException => PromodjRuRating.Unknown
     }
   }
 
   private def findDjPageUrl(artistName: String): Option[String] = {
     val request = url("http://promodj.ru/search/?mode=user&sortby=rating&searchfor=" + Request.encode_%(artistName))
-    PromodjRuRatingParser.parseDjUrl(artistName, Jsoup.parse(httpClient(request as_str)))
+    parseDjUrl(artistName, Jsoup.parse(httpClient(request as_str)))
   }
 }
-
