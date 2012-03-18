fa205559fecb5f28b514207d1d53a5e896cd4771
unknown <artem@artem-laptop.(none)>
2012-03-19 01:44:44 +0400

    Initial working web stuff

---
+++ src/main/scala/sh/app/topartist/mvc/model/RawLineUp.scala
@@ -0,0 +1,9 @@
+package sh.app.topartist.mvc.model
+
+import javax.faces.bean._
+import scala.reflect.BeanProperty
+
+@ManagedBean @SessionScoped
+class RawLineUp extends Serializable {
+  @BeanProperty var content = "abcd"
+}
