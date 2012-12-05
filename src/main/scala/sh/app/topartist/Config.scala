35927e89047dc0ee12821606e67767404382a1af
Artem Shiyanov <shiyanov@gmail.com>
2012-12-06 00:56:41 +0400

    introduce TotalRatingRetriever - simple way to get total rating for the concrete artist

--- src/main/scala/sh/app/topartist/Config.scala
+++ src/main/scala/sh/app/topartist/Config.scala
@@ -5,6 +5,7 @@ import sh.app.topartist.festival.Kazantip
 import sh.app.topartist.rating.lastfm.LastFmRatingRetriever
 import sh.app.topartist.rating.promodjru.PromodjRuRatingRetriever
 import sh.app.topartist.rating.vkontakteru.VkontakteRuRatingRetriever
+import sh.app.topartist.rating.TotalRatingRetriever
 
 
 object Config {
@@ -12,5 +13,6 @@ object Config {
   lazy val lastFmRatingRetriever = new LastFmRatingRetriever(this)
   lazy val promodjRuRatingRetriever = new PromodjRuRatingRetriever(this)
   lazy val vkontakteRuRatingRetriever = new VkontakteRuRatingRetriever(this)
+  lazy val totalRatingRetriever = new TotalRatingRetriever(this)
   lazy val kazantip = new Kazantip(this)
 }
