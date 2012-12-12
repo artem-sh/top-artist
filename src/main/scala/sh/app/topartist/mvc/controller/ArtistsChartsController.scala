e3d4a19286e1fff2abccaa19a464b338afab3cbd
Artem Shiyanov <shiyanov@gmail.com>
2012-12-12 15:21:28 +0400

    refactor charts to support arbitrary count of artists

--- src/main/scala/sh/app/topartist/mvc/controller/ArtistsChartsController.scala
+++ src/main/scala/sh/app/topartist/mvc/controller/ArtistsChartsController.scala
@@ -5,15 +5,19 @@ import sh.app.topartist.Config
 import sh.app.topartist.mvc.model._
 import javax.inject.{Inject, Named}
 import javax.enterprise.context.SessionScoped
-import sh.app.topartist.rating.TotalRating
+import sh.app.topartist.rating.{Rating, TotalRating}
 import javax.faces.event.AjaxBehaviorEvent
 import reflect.BeanProperty
+import sh.app.topartist.rating.lastfm.LastFmRating
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import sh.app.topartist.rating.promodjru.PromodjRuRating
+import sh.app.topartist.rating.vkontakteru.VkontakteRuRating
 
 
 @Named @SessionScoped
 class ArtistsChartsController extends Serializable {
   @Inject private var artistsForm: ArtistsForm = _
-  @Inject private var charts: Charts = _
+  @Inject private var chartss: Charts = _
 
   def process_old(): String = {
     //    artistsRating.rating1 = new TotalRatingWrapper(artistsForm.getArtist1, Config.totalRatingRetriever.retrieveRating(artistsForm.getArtist1))
@@ -27,12 +31,28 @@ class ArtistsChartsController extends Serializable {
   }
 
   def compareArtists(event: AjaxBehaviorEvent) {
-    import collection.JavaConversions._
-    charts.charts = List(new Chart("vk.com", "tracks", artistsForm.artist1, 100, artistsForm.artist2, 150),
-      new Chart("last.fm", "listeners", artistsForm.artist1, 200, artistsForm.artist2, 250))
+    chartss.charts.put(RatingProvider.LastFm, java.util.Arrays.asList(new ArtistRating("a1", 100)))
+    chartss.charts.put(RatingProvider.PromodjRu, java.util.Arrays.asList(new ArtistRating("a1", 200)))
+    chartss.charts.put(RatingProvider.VkontakteRu, java.util.Arrays.asList(new ArtistRating("a1", 300)))
 
     val artist: String = artistsForm.getArtist1
     artistsForm.setArtist1(artistsForm.getArtist2)
     artistsForm.setArtist2(artist)
   }
+
+  private def extractChartFromRating(artist1: String, rating1: Rating, artist2: String, rating2: Rating) {
+    val ratingProvider: RatingProvider = rating1 match {
+      case _: LastFmRating => new RatingProvider("last.fm", "listeners")
+      case _: PromodjRuRating => new RatingProvider("promodj.ru", "rank")
+      case _: VkontakteRuRating => new RatingProvider("vk.com", "tracks")
+    }
+  }
+
+//  private def transformRatingsToCharts(ratings: Map[String, TotalRating]) {
+//    ratings.foreach(a2r => {
+//      chartss.charts(RatingProvider.LastFm) :+ new ArtistRating(a2r._1, a2r._2.lastFmRating.listeners)
+//      chartss.charts(RatingProvider.PromodjRu) :+ new ArtistRating(a2r._1, a2r._2.promodjRuRating.promoRank)
+//      chartss.charts(RatingProvider.VkontakteRu) :+ new ArtistRating(a2r._1, a2r._2.vkontakteRuRating.tracksCount)
+//    })
+//  }
 }
